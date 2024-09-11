ed13f460c2ac5706971bc2dc2f9eb873f19f9281
kperk43 <kperk43@lsu.edu>
2024-09-10 19:37:46 -0500

    Added third java file that calculates the range of an ordered array.

---
+++ prog3.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+public class prog3 {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5,6};
+
+        int range = num[num.length - 1] - num[0];
+
+        System.out.println(range);
+    }
+}
