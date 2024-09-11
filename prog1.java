f77dfab34e6e52bb21706cbd88e51f78094f85af
kperk43 <kperk43@lsu.edu>
2024-09-10 19:36:03 -0500

    Added java file that calculates the sum between two numbers.

---
+++ prog1.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+public class prog1 {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 10;
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
