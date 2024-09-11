441848eadb93b8eb81d30d4aae196921085ecd0d
kperk43 <kperk43@lsu.edu>
2024-09-10 19:36:55 -0500

    Added second java file that calculates the avg of two numbers.

---
+++ prog2.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+public class prog2 {
+    public static void main(String[] args) {
+        int a = 36;
+        int b = 48;
+        int avg = (a + b) / 2;
+
+        System.out.println(avg);
+    }
+}
